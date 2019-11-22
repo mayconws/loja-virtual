5ecec522a43d8583c5fbee9aac04cfc080202057
maycon <mayconwas2018@gmail.com>
2019-11-22 18:54:03 -0200

    Correções no template do cliente

--- virtual/src/main/java/com/loja/virtual/controllers/InicioController.java
+++ virtual/src/main/java/com/loja/virtual/controllers/InicioController.java
@@ -1,14 +1,23 @@
 package com.loja.virtual.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.loja.virtual.repository.ProdutoRepository;
 
 @Controller
 public class InicioController {
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	@GetMapping("/inicio")
-	public String homeAdm() {
-		return "loja/index";
+	public ModelAndView index() {
+		ModelAndView mv =  new ModelAndView("loja/index");	
+		mv.addObject("produto", produtoRepository.findAll());
+		return mv;
 	}
 
 }
